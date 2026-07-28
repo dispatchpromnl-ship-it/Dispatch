f428808c82947baf50cff476475066ef5a5e52c2
dispatchpromnl-ship-it <dispatchpro.mnl@gmail.com>
2026-07-28 13:31:41 +0800

    fix: backward-compatible SHA-256 password verification, fix engines field

--- api/_lib/hash.js
+++ api/_lib/hash.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const SALT_ROUNDS = 10;
 
@@ -6,8 +7,15 @@ function hashPassword(password) {
   return bcrypt.hashSync(password, SALT_ROUNDS);
 }
 
+function sha256(str) {
+  return crypto.createHash('sha256').update(str).digest('hex');
+}
+
 function verifyPassword(password, hash) {
-  return bcrypt.compareSync(password, hash);
+  if (hash.startsWith('$2a$') || hash.startsWith('$2b$')) {
+    return bcrypt.compareSync(password, hash);
+  }
+  return sha256(password) === hash;
 }
 
 module.exports = { hashPassword, verifyPassword };
